6c9f6d7a47bfeacf42c0116c2cc33fc3ccfda921
ssl <1659412599@qq.com>
2021-07-07 21:06:26 +0800

    wanyi

--- 01_JavaBasic/day01_code/src/HelloWorld.java
+++ 01_JavaBasic/day01_code/src/HelloWorld.java
@@ -2,6 +2,6 @@
 // 类是Java当中所有源代码的基本组织单位
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        System.out.println("Hello,World!!!");
     }
 }
